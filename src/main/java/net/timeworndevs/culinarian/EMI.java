09f1abaa620da5f82bac4b686f2a11b649ab3ab1
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2023-12-17 12:57:10 -0600

    hrm

--- src/main/java/net/timeworndevs/culinarian/EMI.java
+++ src/main/java/net/timeworndevs/culinarian/EMI.java
@@ -2,10 +2,12 @@ package net.timeworndevs.culinarian;
 
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import net.minecraft.util.Identifier;
 
 public class EMI implements EmiPlugin {
+
     @Override
     public void register(EmiRegistry registry) {
-        //todo add EMI support
+
     }
 }
